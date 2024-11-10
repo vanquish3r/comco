21feb4c59cd1524c4b93b934c071ba1eadd1c27f
Vanquish3r <153280214+vanquish3r@users.noreply.github.com>
2024-11-10 03:11:08 -0700

    Update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -1,26 +1,29 @@
 const comcoscene = BS.BanterScene.GetInstance();
 
-let youtubePlaylist = `PLpaueZh8o4LYRuMOtuhrHaiahdGdMIslL`;
 let websiteurl = "https://screen.sdq.st:8443/?room=comco"; /* ?autoplay=1&controls=0 For YouTube Live */
-let otherwebsiteurl = "https://comcotheatre.com/";
 
-BS.BanterScene.GetInstance().On("loaded", () => {
-	    console.log("Scene loaded");
-      /* COMMENTED THIS OUT TO REMOVE THE YOUTUBE PLAYER */
-			 enableYouTube();
-      /* UNCOMMENTED THIS TO ENABLE SCREEN CAST / YOUTUBE LIVE */
-	          	// enableTheFireScreen();
-      /* UNCOMMENTED THIS TO ENABLE FIRE TABLET */
-	          	 enableThePortableFireScreen()
-});
+let otherwebsiteurl = "https://comcotheatre.com/"; // Small Screen Up Stairs
+
+async function somerandomStartActions() {
+	const waitingForUnity = async () => { while (!comcoscene.unityLoaded) { await new Promise(resolve => setTimeout(resolve, 500)); } };
+	await waitingForUnity(); console.log("SCRIPT: Unity-Loaded"); setSceneSettings(); setTimeout(() => { 
+
+		/* COMMENTED THIS OUT TO REMOVE THE YOUTUBE PLAYER */
+			 enableVideoPlayer2();
+		/* UNCOMMENTED THIS TO ENABLE SCREEN CAST / YOUTUBE LIVE */
+			// enableScreenStuff2();
+
+    setTimeout(() => { enableScreenThingy(); }, 5000);
+	}, 1000);
+};
 
 async function setSceneSettings() {
 	console.log("setSceneSettings Loading...");
 	const settings = new BS.SceneSettings();
 	settings.EnableDevTools = false;
-	settings.EnableTeleport = false;
+	settings.EnableTeleport = true;
 	settings.EnableForceGrab = false;
-	settings.EnableSpiderMan = true;
+	settings.EnableSpiderMan = false;
 	settings.EnablePortals = true;
 	settings.EnableGuests = true;
 	// settings.EnableQuaternionPose = false;
@@ -37,11 +40,12 @@ async function setSceneSettings() {
 	setTimeout(() => { comcoscene.SetSettings(settings); }, 2000);
 };
 
-// videoplayer toggle by HBR & FireRat.
+// videoplayer toggle by HBR.& Fire Thank you HBR!
 let ytplayerdisabled = true;
-  function enableYouTube() {
-  if (ytplayerdisabled){ ytplayerdisabled = false;
-    console.log("YouTube Player Loading");
+  function enableVideoPlayer2() {
+  if (ytplayerdisabled){
+    console.log("yt player enabling");
+    ytplayerdisabled = false;
     const videoplayer = document.createElement("script");
 		videoplayer.id = "comco-videoplayer";
 		videoplayer.setAttribute("scale", "1 1 1");
@@ -57,79 +61,92 @@ let ytplayerdisabled = true;
 		videoplayer.setAttribute("spatial-max-distance", "1000");
 		videoplayer.setAttribute("playlist", youtubePlaylist);
 		videoplayer.setAttribute("announce", "false");
-		videoplayer.setAttribute("data-playlist-icon-url", "https://cdn.glitch.global/69f02c8f-d538-43b7-9c66-5d3973208d79/Playlist.png?v=1713028119937");
-		videoplayer.setAttribute("data-vol-up-icon-url", "https://cdn.glitch.global/69f02c8f-d538-43b7-9c66-5d3973208d79/VolUp.png?v=1713028119640");
-		videoplayer.setAttribute("data-vol-down-icon-url", "https://cdn.glitch.global/69f02c8f-d538-43b7-9c66-5d3973208d79/VolDown.png?v=1713028119279");
-		videoplayer.setAttribute("data-mute-icon-url", "https://cdn.glitch.global/69f02c8f-d538-43b7-9c66-5d3973208d79/Mute.png?v=1713028120228");
-		videoplayer.setAttribute("data-skip-forward-icon-url", "https://cdn.glitch.global/69f02c8f-d538-43b7-9c66-5d3973208d79/Forward.png?v=1713028118642");
-		videoplayer.setAttribute("data-skip-backward-icon-url", "https://cdn.glitch.global/69f02c8f-d538-43b7-9c66-5d3973208d79/Backwardsd.png?v=1713028118986");
-		videoplayer.setAttribute("src", "https://best-v-player.glitch.me/playlist.js"); // https://vidya.sdq.st/playlist.js
+		videoplayer.setAttribute("data-playlist-icon-url", "https://cdn.glitch.global/47f0acb4-4420-4f3f-bb01-dba17f8c0edb/Playlist.png?v=1711786451727");
+		videoplayer.setAttribute("data-vol-up-icon-url", "https://cdn.glitch.global/47f0acb4-4420-4f3f-bb01-dba17f8c0edb/Vol_Up.png?v=1711785431096");
+		videoplayer.setAttribute("data-vol-down-icon-url", "https://cdn.glitch.global/47f0acb4-4420-4f3f-bb01-dba17f8c0edb/Vol_Dn.png?v=1711785430202");
+		videoplayer.setAttribute("data-mute-icon-url", "https://cdn.glitch.global/47f0acb4-4420-4f3f-bb01-dba17f8c0edb/Vol_Mute_Off.png?v=1711785430667");
+		videoplayer.setAttribute("data-skip-forward-icon-url", "https://cdn.glitch.global/47f0acb4-4420-4f3f-bb01-dba17f8c0edb/Sync_FW.png?v=1711785429798");
+		videoplayer.setAttribute("data-skip-backward-icon-url", "https://cdn.glitch.global/47f0acb4-4420-4f3f-bb01-dba17f8c0edb/Sync_Bk.png?v=1711785429431");
+		videoplayer.setAttribute("src", "https://vidya.sdq.st/playlist.js"); // https://best-v-player.glitch.me/playlist.js
     document.querySelector("a-scene").appendChild(videoplayer);
-  } else {console.log("YouTube Player Loading...");}
+  } else {console.log("enable yt player called");}
 };
 
-let screenDisabled = true;
-function enableTheFireScreen() {
-  if (screenDisabled){ screenDisabled = false;
-	console.log("Adding Screen Cast");
-	const firescreen = document.createElement("script");
-	firescreen.id = "comco-firescreen";
-	firescreen.setAttribute("scale", "0.76 0.76 1");
-	firescreen.setAttribute("rotation", "0 0 0");
-	firescreen.setAttribute("position", "0 -5 0");
-	firescreen.setAttribute("mipmaps", "0");
-	firescreen.setAttribute("pixelsperunit", "1200");
-	firescreen.setAttribute("width", "1280");
-	firescreen.setAttribute("height", "720");
-	firescreen.setAttribute("castmode", "true");
-	firescreen.setAttribute("backdrop", "false");
-	firescreen.setAttribute("hand-controls", "true");
-	firescreen.setAttribute("announce", "false");
-	firescreen.setAttribute("announce-four-twenty", "false");
-	firescreen.setAttribute("announce-events", "false");
-	firescreen.setAttribute("volume", "0.5");
-	firescreen.setAttribute("button-position", "-0.18 10 10.171");
-	firescreen.setAttribute("website", websiteurl);
-	firescreen.setAttribute("src", "https://firer.at/scripts/firescreen.js");
-	document.querySelector("a-scene").appendChild(firescreen);
+let screenstuffDisabled = true;
+function enableScreenStuff2() {
+  if (screenstuffDisabled){
+		screenstuffDisabled = false;
+		console.log("Adding Screen Cast");
+		const firescreen = document.createElement("script");
+		firescreen.id = "comco-firescreen";
+		firescreen.setAttribute("scale", "1 1 1");
+		firescreen.setAttribute("rotation", "0 0 0");
+		firescreen.setAttribute("screen-rotation", "0 0 0");
+		firescreen.setAttribute("screen-scale", "0.76 0.76 1");
+		firescreen.setAttribute("position", "0 -5 0");
+		firescreen.setAttribute("lock-position", "true");
+		firescreen.setAttribute("mipmaps", "1");
+		firescreen.setAttribute("pixelsperunit", "1600");
+		firescreen.setAttribute("castmode", "true");
+		firescreen.setAttribute("backdrop", "false");
+		firescreen.setAttribute("disable-rotation", "true");
+		firescreen.setAttribute("hand-controls", "true");
+		firescreen.setAttribute("announce", "false");
+		firescreen.setAttribute("announce-events", "false");
+		firescreen.setAttribute("announce-four-twenty", "false");
+		firescreen.setAttribute("volume", "0.25");
+		firescreen.setAttribute("width", "1920");
+		firescreen.setAttribute("height", "1080");
+		firescreen.setAttribute("screen-position", "0 0 0");
+		firescreen.setAttribute("website", websiteurl);
+		firescreen.setAttribute("src", "https://firer.at/scripts/firescreenv2.js");
+		document.querySelector("a-scene").appendChild(firescreen);
+		if (websiteurl.includes("hyperbeam.com/i/")) {
+			setTimeout(async () => { 
+				let theBrowserthingy = await comcoscene.Find(`MyBrowser2`);
+				let thebrowserpart = theBrowserthingy.GetComponent(BS.ComponentType.BanterBrowser);
+				thebrowserpart.RunActions(JSON.stringify({"actions": [{ "actionType": "runscript","strparam1": "const checkbox = document.querySelector(`.p-checkbox-box[role='checkbox']`); const joinButton = document.querySelector('.footer_3Yiou .joinBtn_1TAU6'); if (checkbox) checkbox.click(); if (joinButton) { const observer = new MutationObserver(() => { if (!joinButton.classList.contains('p-disabled')) { joinButton.click(); observer.disconnect(); setTimeout(() => { const skipButton = document.querySelector('.dialog-secondary-btn'); if (skipButton) skipButton.click(); }, 3000); } }); observer.observe(joinButton, { attributes: true, attributeFilter: ['class'] }); }" }]}));
+				setTimeout(async () => {
+					thebrowserpart.RunActions(JSON.stringify({"actions": [{ "actionType": "runscript","strparam1": "var fullscreenButton = document.querySelector(`.p-button.p-component.tu-button.btn-tertiary.btn_2YRyp svg path[d^='M3 3h6.429']`); if (fullscreenButton) { fullscreenButton.closest('button').click(); } setTimeout(async () => { var chatButton = document.querySelector(`.p-button.p-component.tu-button.btn-tertiary.fsChatBtn_2cCyy svg path[d^='M22 22h-2V2h2v20zM2 11h12.17']`); if (chatButton) { chatButton.closest('button').click(); } }, 3500);" }]}));
+				}, 5000);
+			}, 3000);
+		}
   }
-  // setTimeout(() => { 
-  //   let firescreencast = document.getElementById("fires-browser1");
-  //   firescreencast.browser.RunActions(JSON.stringify({"actions":[{"actionType": "click2d","strparam1": "0.5,0.5;"}]}));
-  //   console.log("Click Browser");
-  // }, 3000); 
-    console.log("Screen Stuff enabled");
+	console.log("Screen Stuff enabled: " + screenstuffDisabled);
+};
+
+let otherScreenDisabled = true;
+
+function enableScreenThingy() {
+	console.log("File: loaded");
+	if (otherScreenDisabled) {
+		otherScreenDisabled = false;
+		console.log("Adding Screen");
+		const firescreen = document.createElement("script");
+		firescreen.id = "banter-firescreen";
+		firescreen.setAttribute("scale", "0.7 0.7 1");
+		firescreen.setAttribute("position", "-16.45 1.269 0.75");
+		firescreen.setAttribute("rotation", "0 0 0");		
+		firescreen.setAttribute("mipmaps", "0");
+		firescreen.setAttribute("pixelsperunit", "1200");
+		firescreen.setAttribute("hand-controls", "false");
+		firescreen.setAttribute("announce", "false");
+		firescreen.setAttribute("announce-events", "true");
+		firescreen.setAttribute("announce-four-twenty", "false");
+		firescreen.setAttribute("volume", "0.25");
+		firescreen.setAttribute("width", "1280");
+		firescreen.setAttribute("height", "720");
+		firescreen.setAttribute("website", otherwebsiteurl);
+		firescreen.setAttribute("disable-rotation", "false");
+		firescreen.setAttribute("custom-button01-url", "https://banterlateshow.glitch.me/0-0-shownotes-0-0.txt");
+		firescreen.setAttribute("custom-button01-text", "Show Notes");
+		firescreen.setAttribute("custom-button02-url", "https://banterlateshow.glitch.me/darwinawards.html");
+		firescreen.setAttribute("custom-button02-text", "Darwin Awards");
+		firescreen.setAttribute("custom-button03-url", "https://banterlateshow.com");
+		firescreen.setAttribute("custom-button03-text", "Banter Late Show");
+		firescreen.setAttribute("src", "https://firer.at/scripts/firescreenv2.js");
+		document.querySelector("a-scene").appendChild(firescreen);
+	};
 }
 
-let screenPortableDisabled = true;
-function enableThePortableFireScreen() {
-  if (screenPortableDisabled){ screenPortableDisabled = false;
-   setTimeout(() => { 
-	console.log("Adding Fire Tablet");
-	const firescreen = document.createElement("script");
-	firescreen.id = "comco-portable-firescreen";
-	firescreen.setAttribute("scale", "0.7 0.7 1");
-	firescreen.setAttribute("rotation", "0 0 0");
-	firescreen.setAttribute("position", "-16.45 1.269 0.75");
-	firescreen.setAttribute("mipmaps", "0");
-	firescreen.setAttribute("pixelsperunit", "1200");
-	firescreen.setAttribute("width", "1280");
-	firescreen.setAttribute("height", "720");
-	firescreen.setAttribute("announce", "false");
-	firescreen.setAttribute("announce-events", "true");
-	firescreen.setAttribute("volume", "0.5");
- 	firescreen.setAttribute("backdrop", "true");
-	firescreen.setAttribute("custom-button01-url", "https://banterlateshow.glitch.me/0-0-shownotes-0-0.txt");
-	firescreen.setAttribute("custom-button01-text", "Show Notes");
-	firescreen.setAttribute("custom-button02-url", "https://banterlateshow.glitch.me/darwinawards.html");
-	firescreen.setAttribute("custom-button02-text", "Darwin Awards");
-	firescreen.setAttribute("custom-button03-url", "https://banterlateshow.com");
-	firescreen.setAttribute("custom-button03-text", "Banter Late Show");
-	firescreen.setAttribute("website", otherwebsiteurl);
-	firescreen.setAttribute("src", "https://firer.at/scripts/firescreenv2.js");
-	document.querySelector("a-scene").appendChild(firescreen);
-   }, 5000); 
-  }
-    console.log("Fire Tablet enabled");
-
-}
+setTimeout(() => { somerandomStartActions(); }, 5000);
